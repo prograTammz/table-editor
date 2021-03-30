1a55fda1beb6f3b7413febebc201974682c1ea36
Ahmed Tamer <ahmedtamer1998@gmail.com>
2021-03-30 15:37:42 +0200

    added init table-editor

---
+++ src/app/pages/table-editor/table-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-editor',
+  templateUrl: './table-editor.component.html',
+  styleUrls: ['./table-editor.component.scss']
+})
+export class TableEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
