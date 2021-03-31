8200c83cfa61057421f15a9c2279245a537b5c38
Ahmed Tamer <ahmedtamer1998@gmail.com>
2021-03-31 16:26:06 +0200

    added table editor

--- src/app/pages/table-editor/table-editor.component.ts
+++ src/app/pages/table-editor/table-editor.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostListener, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import { TableQueryService } from 'src/app/services/table-query.service';
+import { Record} from '../../models/model';
 @Component({
   selector: 'app-table-editor',
   templateUrl: './table-editor.component.html',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableEditorComponent implements OnInit {
 
-  constructor() { }
+  loading:boolean;
+  page: number;
+  displayedColumns: string[] = [];
+  records: Record[] = [];
+  dataSource = new MatTableDataSource<Record>(this.records);
 
-  ngOnInit(): void {
+  constructor(private tableQuery: TableQueryService) { 
+    this.generateColumns();
+    this.loading = false;
+    this.page = 0;
   }
 
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  generateColumns() {
+    for (let index = 1; index <= 100; index++) {
+      this.displayedColumns.push(`Column ${index}`);
+    }
+    this.displayedColumns.push('edit');
+  }
+
+  getData() {
+    this.loading = true;
+    this.tableQuery.getHundred(this.page).then( data => {
+    this.records = [ ...this.records, ...data];
+    this.dataSource = new MatTableDataSource(this.records);
+     this.loading = false;
+    })
+  }
+
+  edit(row) {
+    this.loading = true;
+    console.log(row);
+    this.tableQuery.update(row.id, row).then(_ => {
+      this.loading = false;
+    })
+  }
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: any) {
+      // visible height + pixel scrolled >= total height 
+      if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
+        console.log("End");
+      }
+  }
 }
