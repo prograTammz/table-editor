d5f9569e437f412787e70f3a0eb9edeb54cbf133
Ahmed Tamer <ahmedtamer1998@gmail.com>
2021-03-31 14:22:54 +0200

    added login logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  hide = true;
+  constructor(public fb: FormBuilder, private auth: LoginService) { }
 
   ngOnInit(): void {
+    this.constructLoginForm();
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  constructLoginForm(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    this.loginForm.markAllAsTouched();
+    let isValid = this.auth.login(this.f.username.value, this.f.password.value);
+
+    if(!isValid) {
+      this.f.username.setErrors({ loginError: true });
+      this.f.password.setErrors({ loginError: true });
+    }
+  }
+
+  onChange() {
+    this.f.username.setErrors({ loginError: null });
+    this.f.password.setErrors({ loginError: null });
+    this.f.username.updateValueAndValidity();
+    this.f.password.updateValueAndValidity();
   }
 
 }
