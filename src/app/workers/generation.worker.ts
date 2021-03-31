f7cce72f7de536580d84d90b7822d51936881b5f
Ahmed Tamer <ahmedtamer1998@gmail.com>
2021-03-31 10:01:11 +0200

    added generateString function to generation worker

--- src/app/workers/generation.worker.ts
+++ src/app/workers/generation.worker.ts
@@ -1,5 +1,20 @@
 /// <reference lib="webworker" />
 
+//Generates a string from CAPs Alphapet with random value between min & max
+function generateString(min,max) {
+  let result = ' ';
+  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const randomLength = randomIntFromInterval(min,max);
+  const charactersLength = characters.length;
+  for ( let i = 0; i < randomLength; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+
+
 
 addEventListener('message', ({ data }) => {
   const response = `worker response to ${data}`;
